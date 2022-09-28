8cd42e133439bff28f848f8fefc14a8e8c47871b
passportpowell <otisofro@gmail.com>
2022-09-28 14:10:52 +0000

    add validation check for 6 values exactly, add error message

--- run.py
+++ run.py
@@ -1,6 +1,3 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
 import gspread
 from google.oauth2.service_account import Credentials
 
@@ -16,13 +13,27 @@ GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('love_sandwiches')
 
 
-
 def get_sales_data():
-    print("Enter Sales data from today")
-    print("Data should be 6 number, seperated by commas")
-    print("for example: 2, 5, 2, 45, 24, 25\n")
+    """
+    Get sales figures input from the user.
+    """
+    print("Please enter sales data from the last market.")
+    print("Data should be six numbers, separated by commas.")
+    print("Example: 10,20,30,40,50,60\n")
 
-    data_str = input("Data here: ")
-    print(f"Data entered was {data_str}")
+    data_str = input("Enter your data here: ")
+
+    sales_data = data_str.split(",") #cant update spreadsheet values without converting into a list
+    validate_data(sales_data)
+
+def validate_data(values):
+    try:
+        if len(values) !=6:
+            raise ValueError(
+                f"Exactly 6 Values required, you've provided {len(values)}"
+            )
+    except ValueError as e:
+        print(f"Invalid data: {e}, please try again...\n")
+    print(values)
 
 get_sales_data()
